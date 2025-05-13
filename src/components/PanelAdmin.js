347b0c4f5ad78f9ef1a59c6f30bc5bf35b7dbfc4
Jorge Zamudio <epic.fandom.entertainmentgmail.com>
2025-05-13 14:02:49 -0600

    Fix: agregar componente PanelAdmin

---
+++ src/components/PanelAdmin.js
@@ -0,0 +1,10 @@
+// src/components/PanelAdmin.js
+import React from 'react';
+
+export default function PanelAdmin() {
+  return (
+    <div className="text-white">
+      <p>Panel de administración activado.</p>
+    </div>
+  );
+}
